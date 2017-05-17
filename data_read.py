8a2c0af2675d1398d7ec6d9b064a4ba3b3e6c898
left thomas <leftthomas@qq.com>
2017-05-17 21:28:48 +0800

    mask example

--- data_read.py
+++ data_read.py
@@ -1,5 +1,12 @@
+import csv
+import sys
+
+import pandas as pd
 import tifffile as tiff
-from shapely import wkt
+
+csv.field_size_limit(sys.maxsize)
+
+df = pd.read_csv('../data/train_wkt_v4.csv')
 
 img_filename_16bandA = '../data/sixteen_band/6100_1_3_A.tif'
 img_filename_16bandM = '../data/sixteen_band/6100_1_3_M.tif'
@@ -9,6 +16,4 @@ A = tiff.imread(img_filename_16bandA)
 M = tiff.imread(img_filename_16bandM)
 P = tiff.imread(img_filename_16bandP)
 print(A.shape, M.shape, P.shape)
-
-wkt.loads()
 # tiff.imshow(P)
