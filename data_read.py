b0222ab3adccdfaf2d6d7ebf3d88b24755cbaf7d
left thomas <leftthomas@qq.com>
2017-05-17 17:46:02 +0800

    查看geofile数据格式

--- data_read.py
+++ data_read.py
@@ -1,10 +1,14 @@
 import tifffile as tiff
-from matplotlib import pyplot
+from shapely import wkt
 
 img_filename_16bandA = '../data/sixteen_band/6100_1_3_A.tif'
 img_filename_16bandM = '../data/sixteen_band/6100_1_3_M.tif'
 img_filename_16bandP = '../data/sixteen_band/6100_1_3_P.tif'
 
+A = tiff.imread(img_filename_16bandA)
+M = tiff.imread(img_filename_16bandM)
 P = tiff.imread(img_filename_16bandP)
-tiff.imshow(P)
-pyplot.show()
+print(A.shape, M.shape, P.shape)
+
+wkt.loads()
+# tiff.imshow(P)
