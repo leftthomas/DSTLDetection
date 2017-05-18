60ae8df0681de858601740c6c84f9358cafba34d
left thomas <leftthomas@qq.com>
2017-05-18 19:50:21 +0800

    删除一些不需要的代码

--- poly-pixels-ml-poly.py
+++ poly-pixels-ml-poly.py
@@ -7,10 +7,9 @@ import sys
 import numpy as np
 import shapely.affinity
 import tifffile as tiff
-from matplotlib import pyplot as plt
 from shapely import wkt
 
-from mask_utils import mask_for_polygons, get_scalers, mask_to_polygons
+from mask_utils import mask_for_polygons, get_scalers
 
 csv.field_size_limit(sys.maxsize)
 
@@ -44,33 +43,6 @@ train_polygons_scaled = shapely.affinity.scale(
 train_mask = mask_for_polygons(train_polygons_scaled, im_size)
 
 
-# A helper for nicer display
-def scale_percentile(matrix):
-    w, h, d = matrix.shape
-    matrix = np.reshape(matrix, [w * h, d]).astype(np.float64)
-    # Get 2nd and 98th percentile
-    mins = np.percentile(matrix, 1, axis=0)
-    maxs = np.percentile(matrix, 99, axis=0) - mins
-    matrix = (matrix - mins[None, :]) / maxs[None, :]
-    matrix = np.reshape(matrix, [w, h, d])
-    matrix = matrix.clip(0, 1)
-    return matrix
-
-
-# Check that image and mask are aligned. Image:
-tiff.imshow(255 * scale_percentile(im_rgb[2900:3200, 2000:2300]))
-plt.show()
-
-
-# And mask:
-def show_mask(m):
-    # hack for nice display
-    tiff.imshow(255 * np.stack([m, m, m]))
-    plt.show()
-
-
-show_mask(train_mask[2900:3200, 2000:2300])
-
 # Now, let's train a very simple logistic regression classifier,
 # just to get some noisy prediction to show how output mask is processed.
 from sklearn.linear_model import SGDClassifier
@@ -89,37 +61,3 @@ pred_ys = pipeline.predict_proba(xs)[:, 1]
 print('average precision', average_precision_score(ys, pred_ys))
 pred_mask = pred_ys.reshape(train_mask.shape)
 
-# Now check predictions:
-show_mask(pred_mask[2900:3200, 2000:2300])
-
-# We must choose a threshold to turn it into a binary mask:
-threshold = 0.3
-pred_binary_mask = pred_mask >= threshold
-show_mask(pred_binary_mask[2900:3200, 2000:2300])
-
-# Now it's possible to check Jaccard on the pixel level:
-# check jaccard on the pixel level
-tp, fp, fn = ((pred_binary_mask & train_mask).sum(),
-              (pred_binary_mask & ~train_mask).sum(),
-              (~pred_binary_mask & train_mask).sum())
-print('Pixel jaccard', tp / (tp + fp + fn))
-
-
-# Turn our prediction to polygons, and then turn back into a mask to check what it looks like:
-pred_polygons = mask_to_polygons(pred_binary_mask)
-pred_poly_mask = mask_for_polygons(pred_polygons, im_size)
-show_mask(pred_poly_mask[2900:3200, 2000:2300])
-
-# Now to create a submission we just scale back to original coordinates
-scaled_pred_polygons = shapely.affinity.scale(
-    pred_polygons, xfact=1 / x_scaler, yfact=1 / y_scaler, origin=(0, 0, 0))
-
-# Checking submission size:
-dumped_prediction = wkt.dumps(scaled_pred_polygons)
-print('Prediction size: {:,} bytes'.format(len(dumped_prediction)))
-final_polygons = wkt.loads(dumped_prediction)
-
-# Now the litmus test: check Jaccard compared to original polygons
-print('Final jaccard',
-      final_polygons.intersection(train_polygons).area /
-      final_polygons.union(train_polygons).area)
