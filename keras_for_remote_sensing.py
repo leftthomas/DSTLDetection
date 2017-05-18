ae4dc06f353d15623cc6ea9c3485ce472ff27cc5
left thomas <leftthomas@qq.com>
2017-05-18 22:36:45 +0800

    线性分类

--- keras_for_remote_sensing.py
+++ keras_for_remote_sensing.py
@@ -3,14 +3,19 @@ import sys
 
 import numpy as np
 import shapely.affinity
+from keras.layers import Activation
+from keras.layers import Flatten, Dense, Conv2D, MaxPool2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
 from shapely import wkt
+from sklearn.model_selection import train_test_split
 
-from image_utils import RGB
+from image_utils import M
 from mask_utils import mask_for_polygons, get_scalers
 
 csv.field_size_limit(sys.maxsize)
 
-IM_ID = '6070_2_3'
+IM_ID = '6100_2_2'
 POLY_TYPE = '5'  # Trees
 
 # Load grid size
@@ -28,7 +33,7 @@ for _im_id, _poly_type, _poly in csv.reader(open('../data/train_wkt_v4.csv')):
         break
 
 # Read image
-im_rgb = RGB(IM_ID)
+im_rgb = M(IM_ID)
 im_size = im_rgb.shape[:2]
 
 
@@ -54,17 +59,10 @@ for xstart, xend in zip(x[:-1], x[1:]):
 patches = np.array(patch_list)
 lbls = np.array(lbl_list)
 
-from sklearn.model_selection import train_test_split
-
 xtrain, xtest, ytrain, ytest = train_test_split(patches, lbls, train_size=0.8, test_size=0.2)
 
-from keras.layers import Flatten, Dense, Conv2D, MaxPool2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Activation
-from keras.models import Sequential
-
 model = Sequential()
-model.add(Conv2D(64, (1, 1), input_shape=(16, 16, 3), padding="same"))
+model.add(Conv2D(64, (1, 1), input_shape=(16, 16, 8), padding="same"))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
 model.add(MaxPool2D((2, 2)))
@@ -78,7 +76,7 @@ model.add(MaxPool2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(1))
 
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 model.fit(xtrain, ytrain, batch_size=32, epochs=10)
 
--- poly-pixels-ml-poly.py
+++ poly-pixels-ml-poly.py
@@ -1,20 +1,23 @@
-# This script shows the full training and prediction pipeline for a pixel-based classifier:
-# we create a mask, train logistic regression on one-pixel patches, make prediction for all pixels,
-# create and smooth polygons from pixels.
 import csv
 import sys
 
 import numpy as np
 import shapely.affinity
-import tifffile as tiff
+import tifffile
+from matplotlib import pyplot as plt
 from shapely import wkt
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import average_precision_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
+from image_utils import RGB
 from mask_utils import mask_for_polygons, get_scalers
 
 csv.field_size_limit(sys.maxsize)
 
-# We'll work on buildings (class 1) from image 6120_2_2. Fist load grid sizes and polygons.
-IM_ID = '6070_2_3'
+# We'll work on Crops (class 6) from image 6100_2_2. Fist load grid sizes and polygons.
+IM_ID = '6100_2_2'
 POLY_TYPE = '6'  # Crops
 
 # Load grid size
@@ -31,8 +34,7 @@ for _im_id, _poly_type, _poly in csv.reader(open('../data/train_wkt_v4.csv')):
         train_polygons = wkt.loads(_poly)
         break
 
-# Read image with tiff
-im_rgb = tiff.imread('../data/three_band/{}.tif'.format(IM_ID)).transpose([1, 2, 0])
+im_rgb = RGB(IM_ID)
 im_size = im_rgb.shape[:2]
 
 x_scaler, y_scaler = get_scalers(im_size, x_max, y_min)
@@ -42,17 +44,17 @@ train_polygons_scaled = shapely.affinity.scale(
 
 train_mask = mask_for_polygons(train_polygons_scaled, im_size)
 
-
 # Now, let's train a very simple logistic regression classifier,
-# just to get some noisy prediction to show how output mask is processed.
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import average_precision_score
-
+# just to get some noisy prediction to show how output mask is processed
 xs = im_rgb.reshape(-1, 3).astype(np.float32)
 ys = train_mask.reshape(-1)
 pipeline = make_pipeline(StandardScaler(), SGDClassifier(loss='log'))
+
+
+def show_mask(m):
+    # hack for nice display
+    tifffile.imshow(255 * np.stack([m, m, m]))
+    plt.show()
 
 print('training...')
 # do not care about overfitting here
@@ -60,4 +62,7 @@ pipeline.fit(xs, ys)
 pred_ys = pipeline.predict_proba(xs)[:, 1]
 print('average precision', average_precision_score(ys, pred_ys))
 pred_mask = pred_ys.reshape(train_mask.shape)
-
+show_mask(pred_mask[2900:3200, 2000:2300])
+threshold = 0.3
+pred_binary_mask = pred_mask >= threshold
+show_mask(pred_binary_mask[2900:3200, 2000:2300])
