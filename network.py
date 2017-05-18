8b9ba1cf5e42be8da83414af3210d065d2ab14c6
left thomas <leftthomas@qq.com>
2017-05-18 22:59:14 +0800

    分拆类

---
+++ network.py
@@ -0,0 +1,105 @@
+import numpy as np
+from keras import backend as K
+from keras.layers import Input, Conv2D, MaxPool2D, UpSampling2D, concatenate
+from keras.models import Model
+from keras.optimizers import Adam
+from sklearn.metrics import jaccard_similarity_score
+
+N_Cls = 10
+ISZ = 160
+smooth = 1e-12
+# Theano dimension ordering in this code
+K.set_image_dim_ordering('th')
+
+
+def get_unet():
+    inputs = Input((8, ISZ, ISZ))
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPool2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPool2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(256, (3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPool2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(pool4)
+    conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(conv5)
+
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=1)
+    conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(up6)
+    conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(conv6)
+
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=1)
+    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(up7)
+    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv7)
+
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=1)
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(up8)
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv8)
+
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=1)
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(up9)
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv9)
+
+    conv10 = Conv2D(N_Cls, (1, 1), activation="sigmoid")(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=[jaccard_coef, jaccard_coef_int, 'accuracy'])
+    return model
+
+
+def jaccard_coef(y_true, y_pred):
+    intersection = K.sum(y_true * y_pred, axis=[0, -1, -2])
+    sum_ = K.sum(y_true + y_pred, axis=[0, -1, -2])
+
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+
+    return K.mean(jac)
+
+
+def jaccard_coef_int(y_true, y_pred):
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+
+    intersection = K.sum(y_true * y_pred_pos, axis=[0, -1, -2])
+    sum_ = K.sum(y_true + y_pred_pos, axis=[0, -1, -2])
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return K.mean(jac)
+
+
+def calc_jacc(model):
+    img = np.load('data/x_tmp_%d.npy' % N_Cls)
+    msk = np.load('data/y_tmp_%d.npy' % N_Cls)
+
+    prd = model.predict(img, batch_size=4)
+    print(prd.shape, msk.shape)
+    avg, trs = [], []
+
+    for i in range(N_Cls):
+        t_msk = msk[:, i, :, :]
+        t_prd = prd[:, i, :, :]
+        t_msk = t_msk.reshape(msk.shape[0] * msk.shape[2], msk.shape[3])
+        t_prd = t_prd.reshape(msk.shape[0] * msk.shape[2], msk.shape[3])
+
+        m, b_tr = 0, 0
+        for j in range(10):
+            tr = j / 10.0
+            pred_binary_mask = t_prd > tr
+
+            jk = jaccard_similarity_score(t_msk, pred_binary_mask)
+            if jk > m:
+                m = jk
+                b_tr = tr
+        print(i, m, b_tr)
+        avg.append(m)
+        trs.append(b_tr)
+
+    score = sum(avg) / 10.0
+    return score, trs
