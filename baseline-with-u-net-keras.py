acad6bfa451b3870c611ef7416aca205fc812f16
left thomas <leftthomas@qq.com>
2017-05-18 14:27:14 +0800

    完成baseline的unet

--- baseline-with-u-net-keras.py
+++ baseline-with-u-net-keras.py
@@ -26,6 +26,8 @@ SB = pd.read_csv(os.path.join(inDir, 'sample_submission.csv'))
 ISZ = 160
 smooth = 1e-12
 
+K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
+
 
 def _convert_coordinates_to_raster(coords, img_size, xymax):
     Xmax, Ymax = xymax
@@ -202,7 +204,7 @@ def make_val():
 
 
 def get_unet():
-    inputs = Input((ISZ, ISZ, 8))
+    inputs = Input((8, ISZ, ISZ))
     conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
     conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
     pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
@@ -222,25 +224,25 @@ def get_unet():
     conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(pool4)
     conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(conv5)
 
-    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=-1)
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=1)
     conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(up6)
     conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(conv6)
 
-    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=-1)
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=1)
     conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(up7)
     conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv7)
 
-    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=-1)
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=1)
     conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(up8)
     conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv8)
 
-    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=-1)
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=1)
     conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(up9)
     conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv9)
 
     conv10 = Conv2D(N_Cls, (1, 1), activation="sigmoid")(conv9)
 
-    model = Model(input=inputs, output=conv10)
+    model = Model(inputs=inputs, outputs=conv10)
     model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=[jaccard_coef, jaccard_coef_int, 'accuracy'])
     return model
 
