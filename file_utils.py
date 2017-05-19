e6b11c1f19d7bb49dd70f00c349ace8a653b69e3
left thomas <leftthomas@qq.com>
2017-05-19 10:07:47 +0800

    完成geojson的polys读取

---
+++ file_utils.py
@@ -0,0 +1,28 @@
+import json
+
+from shapely.geometry import Polygon
+
+classType_to_filename = {
+    1: '006_VEG_L2_SCRUBLAND',  # 灌木丛
+    2: '006_VEG_L5_GROUP_TREES',  # 林地，群树
+    3: '006_VEG_L5_STANDALONE_TREES',  # 单树
+    4: '007_AGR_L2_CONTOUR_PLOUGHING_CROPLAND',  # 耕地，农田
+    5: '007_AGR_L2_ORCHARD',  # 果园
+    6: '007_AGR_L6_ROW_CROP'  # 农作物
+}
+
+
+# 读取指定的img对应的class的geojson文件的polygons
+def load_geojson_to_polygons(img_id, class_type):
+    file = json.load(open('../data/train_geojson_v3/' + img_id + '/' +
+                          classType_to_filename.get(class_type) + '.geojson'))
+    # print(file)
+    polygon_list = list()
+    for feature in file['features']:
+        # print(len(feature['geometry']['coordinates'][0]))
+        # print(Polygon(feature['geometry']['coordinates'][0]))
+        polygon_list.append(Polygon(feature['geometry']['coordinates'][0]))
+    # print(polygon_list)
+    return polygon_list
+
+# load_geojson_to_polygons('6100_2_2', 6)
