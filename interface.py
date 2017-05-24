e52d3313d9145296fa883a0131507ae0562a045c
left thomas <leftthomas@qq.com>
2017-05-24 14:20:34 +0800

    完成界面设计的mask生成部分

--- interface.py
+++ interface.py
@@ -1,7 +1,9 @@
 import tkinter.messagebox as messagebox
 from tkinter import *
 
+from file_utils import *
 from image_utils import *
+from mask_utils import *
 
 
 class Application(Frame):
@@ -20,13 +22,24 @@ class Application(Frame):
         self.image_type = Entry(self)
         self.image_type.grid(row=3, column=1)
 
-        Label(self, text="----------------------------------数据处理----------------------------------").grid(row=4,
-                                                                                                          column=0,
-                                                                                                          columnspan=2)
+        Label(self, text="----------------------------------数据集处理----------------------------------").grid(row=4,
+                                                                                                           column=0,
+                                                                                                           columnspan=2)
         Button(self, text='训练集选择说明', command=self.show_train_info).grid(row=5, column=0)
         Button(self, text='数据集处理说明', command=self.show_data_info).grid(row=5, column=1)
         Button(self, text='显示处理后的图像（M8波段）', command=self.show_handled_m_image).grid(row=6, column=0)
         Button(self, text='显示处理后的图像（提取RGB波段）', command=self.show_handled_rgb_image).grid(row=6, column=1)
+
+        Label(self, text="----------------------------------mask生成----------------------------------").grid(row=7,
+                                                                                                            column=0,
+                                                                                                            columnspan=2)
+        Button(self, text='mask生成说明', command=self.show_mask_info).grid(row=8, column=0)
+        Button(self, text='植被种类说明', command=self.show_vegs_info).grid(row=8, column=1)
+        Label(self, text="植被种类，默认为2：").grid(row=9, column=0)
+        self.image_class = Entry(self)
+        self.image_class.grid(row=9, column=1)
+        Button(self, text='显示多边形区域', command=self.show_polygons).grid(row=10, column=0)
+        Button(self, text='显示mask', command=self.show_mask).grid(row=10, column=1)
 
     def show_info(self):
         messagebox.showinfo('遥感数据集说明', '原始遥感图像数据集分为两种，一种是3波段的RGB图像，另一种是16'
@@ -90,6 +103,62 @@ class Application(Frame):
         except:
             messagebox.showinfo('提示', '输入图像ID有误')
 
+    def show_mask_info(self):
+        messagebox.showinfo('mask生成说明', '因为数据集的训练数据是以多边形区域（MultiPolygon）的形式提供的，所以需要'
+                                        '先将其转为mask再输入神经网络中进行训练。')
+
+    def show_vegs_info(self):
+        messagebox.showinfo('植被种类说明', '1:灌木丛；2:林地；3:单树；4:耕地；5:果园；6:农作物。')
+
+    def show_polygons(self):
+        img_id = self.image_id.get() or '6100_2_2'
+        img_class = self.image_class.get() or '2'
+        try:
+            m = M(img_id)
+            image = np.zeros((m.shape[0], m.shape[1], 3))
+            image[:, :, 0] = m[:, :, 4]
+            image[:, :, 1] = m[:, :, 2]
+            image[:, :, 2] = m[:, :, 1]
+            image = stretch_n(image)
+            if img_class in ['1', '2', '3', '4', '5', '6']:
+                # 从geojson文件中读取polygons
+                polygons_geojson = load_geojson_to_polygons(img_id, int(img_class))
+                # 从grid_sizes.csv中读取指定图像的 Xmax 和 Ymin
+                x_max, y_min = get_xmax_ymin(img_id)
+                # 得到polygons地理坐标到像素坐标点映射的缩放因子
+                x_scale, y_scale = get_scales((image.shape[0], image.shape[1]), x_max, y_min)
+                # polys的绘制
+                display_polygons(polygons_geojson, image, x_scale, y_scale)
+            else:
+                messagebox.showinfo('提示', '输入植被种类有误')
+        except:
+            messagebox.showinfo('提示', '输入图像ID有误')
+
+    def show_mask(self):
+        img_id = self.image_id.get() or '6100_2_2'
+        img_class = self.image_class.get() or '2'
+        try:
+            m = M(img_id)
+            image = np.zeros((m.shape[0], m.shape[1], 3))
+            image[:, :, 0] = m[:, :, 4]
+            image[:, :, 1] = m[:, :, 2]
+            image[:, :, 2] = m[:, :, 1]
+            image = stretch_n(image)
+            if img_class in ['1', '2', '3', '4', '5', '6']:
+                # 从geojson文件中读取polygons
+                polygons_geojson = load_geojson_to_polygons(img_id, int(img_class))
+                # 从grid_sizes.csv中读取指定图像的 Xmax 和 Ymin
+                x_max, y_min = get_xmax_ymin(img_id)
+                # 得到polygons地理坐标到像素坐标点映射的缩放因子
+                x_scale, y_scale = get_scales((image.shape[0], image.shape[1]), x_max, y_min)
+                # 将polygons转化成mask
+                mask = polygons_to_mask(polygons_geojson, (image.shape[0], image.shape[1]), False, x_scale, y_scale)
+                # 测试mask的绘制
+                display_img(mask)
+            else:
+                messagebox.showinfo('提示', '输入植被种类有误')
+        except:
+            messagebox.showinfo('提示', '输入图像ID有误')
 
 app = Application()
 # 设置窗口标题:
